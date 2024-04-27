7c73f7eac336e89e844ccc064cc2dc8723f618e2
Doritoso.o <meyesblue@gmail.com>
2024-04-27 14:43:47 -0400

    uwu #2

--- core/views.py
+++ core/views.py
@@ -4,22 +4,26 @@ from .utils import cambio_moneda
 
 from django.views.decorators.csrf import csrf_exempt
 
+#VARIABLES CAMBIO MONEDA
+dolar=int(float(cambio_moneda("F073.TCO.PRE.Z.D")))
+euro=int(float(cambio_moneda("F072.CLP.EUR.N.O.D")))
+
+
 # Create your views here.
 @csrf_exempt
 def index (request):
     herramientas = Herramienta.objects.all()
+    cambio_moneda("F073.TCO.PRE.Z.D")
     if request.method == 'POST':
         moneda = request.POST.get('moneda')
         print("valor moneda: ",moneda)
         if moneda == '2': 
-            cod = "F073.TCO.PRE.Z.D"
             for herramienta in herramientas:
-                herramienta.precio = round(herramienta.precio / int(float(cambio_moneda(cod))), 2)
+                herramienta.precio = round((herramienta.precio / dolar),2)
                 print("precio: ",herramienta.precio)
         elif moneda == '3': 
-            cod = "F072.CLP.EUR.N.O.D"
             for herramienta in herramientas:
-                herramienta.precio = round(herramienta.precio / int(float(cambio_moneda(cod))), 2)
+                herramienta.precio = round((herramienta.precio / euro),2)
                 print("precio: ",herramienta.precio)
         else :
             for herramienta in herramientas:
