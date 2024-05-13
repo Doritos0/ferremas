fbf2ca6378658d08c12e5d646139b2d8a9c7d2d2
Doritoso.o <meyesblue@gmail.com>
2024-05-13 01:14:56 -0400

    uwu #14

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ from .utils import cambio_moneda
 import requests
 from django.views.decorators.csrf import csrf_exempt
 
+from datetime import datetime
+
 #VARIABLES CAMBIO MONEDA
 dolar=int(float(cambio_moneda("F073.TCO.PRE.Z.D")))
 euro=int(float(cambio_moneda("F072.CLP.EUR.N.O.D")))
@@ -34,12 +36,31 @@ def index (request):
 
 def index(request):
 
-    url = 'http://127.0.0.1:8001/lista_productos/'
+    fecha_actual = datetime.now().strftime('%Y/%m/%d')
+
+    url = 'http://127.0.0.1:8001/'
+    url_productos = url+'lista_productos/'
+    url_tipos = url+'lista_tipos/'
+    url_stocks = url+'lista_stocks/'
+    url_precios = url+'lista_precios/'
+
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            productos = [type('', (object,), item)() for item in data]
+        response_productos = requests.get(url_productos)
+        response_tipos = requests.get(url_tipos)
+        response_stocks = requests.get(url_stocks)
+        response_precios = requests.get(url_precios)
+
+        if response_productos.status_code == 200:
+            data_productos = response_productos.json()
+            data_tipos = response_tipos.json()
+            data_stocks = response_stocks.json()
+            data_precios = response_precios.json()
+            
+            productos = [type('', (object,), item)() for item in data_productos]
+            tipos = [type('', (object,), item)() for item in data_tipos]
+            stocks = [type('', (object,), item)() for item in data_stocks]
+            precios = [type('', (object,), item)() for item in data_precios]
+
             if request.method == 'POST':
                 moneda = request.POST.get('moneda')
                 print("valor moneda: ",moneda)
@@ -55,7 +76,7 @@ def index(request):
                     for herramienta in productos:
                         herramienta.precio = herramienta.precio
 
-            return render(request, 'core/index.html', {'herra': productos})
+            return render(request, 'core/index.html', {'herra': productos, 'tipos':tipos, 'stocks': stocks,'precios': precios, 'fecha_actual': fecha_actual})
         else:
             print('Error al consultar la API')
             return render(request, 'core/error.html')
